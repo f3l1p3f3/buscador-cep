8d653ada3ac23d13afc983b0436b3fc6ba4e192b
felipe <felipe.ferreirag3@gmail.com>
2022-09-04 11:54:26 -0300

    customização do alert

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import { FiSearch } from 'react-icons/fi'
+import './styles.css'
+import { useState } from 'react'
+import api from './services/api'
+import { toast } from 'react-toastify';
+
+
+function App() {
+  const [input, setInput] = useState('')
+  const [cep, setCep] = useState({})
+
+  async function handleSearch() {
+
+    if (input === '') {
+      toast.info("Preencha um CEP")
+      return;
+    }
+    try {
+      const response = await api.get(`${input}/json`)
+
+      if (response.data.erro) {
+        toast.error("Ops!! CEP informado não encontrado.")
+        setInput('')
+        return;
+      }
+
+      setCep(response.data)
+      const btn = document.querySelector('button.btnLimpar')
+      btn.style.display = "block"
+
+      setInput('')
+
+    } catch {
+      toast.error("Ops!! erro ao buscar CEP")
+      setInput('')
+    }
+
+  }
+
+  function cleanMain() {
+    setCep('')
+    const btn = document.querySelector('button.btnLimpar')
+    btn.style.display = ""
+
+  }
+
+  return (
+    <>
+      <div className="container">
+        <h1 className="title">Buscador de CEP</h1>
+        <div className="containerInput">
+          <input type='text'
+            placeholder="Digite seu CEP"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+
+          <button className="buttonSearch" onClick={handleSearch}>
+            <FiSearch size={25} color='#FFF' />
+          </button>
+        </div>
+
+
+        {Object.keys(cep).length > 0 && (
+
+          <main className='main'>
+
+            <h2>CEP: {cep.cep}</h2>
+
+            <span>Rua: {cep.logradouro}</span>
+            <span>Complemento: {cep.complemento}</span>
+            <span>{cep.bairro}</span>
+            <span>{cep.logradouro} - {cep.uf}</span>
+          </main>
+        )
+        }
+
+        <button className='btnLimpar' onClick={cleanMain}>Limpar</button>
+
+      </div>
+    </>
+
+  );
+}
+
+export default App;
